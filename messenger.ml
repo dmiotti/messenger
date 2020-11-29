f863b1a7cef1702bf16f9d767d41111953fd2128
David Miotti <davidm@doist.com>
2020-11-29 09:49:55 +0100

    Add basic apis

--- messenger.ml
+++ messenger.ml
@@ -1,9 +1,42 @@
-open Opium.Std
+open Opium
 
-let health_handler _ =
-  `String "I‘m healthy" |> respond'
+module Person = struct
+  type t =
+    { name : string
+    ; age : int
+    }
+  [@@deriving yojson { exn = true }]
+end
 
-let () =
+let print_person_handler req =
+  let name = Router.param req "name" in
+  let age = Router.param req "age" |> int_of_string in
+  let person = { Person.name; age } |> Person.to_yojson in
+  Lwt.return (Response.of_json person)
+
+let update_person_handler req =
+  let open Lwt.Syntax in
+  let+ json = Request.to_json_exn req in
+  let person = Person.of_yojson_exn json in
+  Logs.info (fun m -> m "Received person: %s" person.Person.name);
+  Response.of_json (`Assoc [ "message", `String "Person saved" ])
+
+let streaming_handler req =
+  let length = Body.length req.Request.body in
+  let content = Body.to_stream req.Request.body in
+  let body = Lwt_stream.map String.uppercase_ascii content in
+  Response.make ~body:(Body.of_stream ?length body) () |> Lwt.return
+
+let print_param_handler req =
+  Printf.sprintf "Hello, %s\n" (Router.param req "name")
+  |> Response.of_plain_text
+  |> Lwt.return
+
+let _ =
   App.empty
-  |> (get "/" health_handler)
+  |> App.post "/hello/stream" streaming_handler
+  |> App.get "/hello/:name" print_param_handler
+  |> App.get "/person/:name/:age" print_person_handler
+  |> App.patch "/person" update_person_handler
   |> App.run_command
+;;
